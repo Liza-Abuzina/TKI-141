d45d0baec05156299fa2bcd74cee126d769580c5
Liza-Abuzina <linkschannel220@gmail.com>
2024-10-06 20:19:09 +0300

    код на проверку

---
+++ Tasks3/Task2-2/2-2.c
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+/**
+*@brief Рассчитывает выражение a по формуле
+*@param a - Значение константы a
+*@param x - Значение вводимого числа пользователем
+*@return Рассчитаное значение a
+*/
+double getY(const double a, const double x);
+
+/**
+* @brief Считывает вещественное число
+* @return Вещественное число
+*/
+double Input(void);
+
+/**
+* @brief Точка входа в программу =
+* @return Код ошибки
+*/
+int main(void)
+{
+	setlocale(LC_ALL, "Russian");
+	const double a = 2.5;
+	puts("Введите число");
+	double x = Input();
+	double Y = getY(a, x);
+	printf_s("Результат вычислений %lf", Y);
+	return 0;
+}
+
+double getY(const double a, const double x)
+{
+	if (x > a)
+		return x * cbrt(x - a);
+	else
+		return x * sin(a * x);
+}
+
+double Input(void)
+{
+	double value;
+	scanf_s("%lf", &value);
+	return value;
+}
