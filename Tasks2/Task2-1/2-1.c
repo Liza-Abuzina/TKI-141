3ac2bd93c7716e17621934c7ffa322d2c48be9ff
Liza-Abuzina <linkschannel220@gmail.com>
2024-10-06 19:31:29 +0300

    код на проверку

---
+++ Tasks2/Task2-1/2-1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <locale.h>
+
+/**
+*@brief Рассчитывает выражение a по формуле
+*@param radius - Значение радиуса шара
+*@return Рассчитаное значение a
+*/
+double getVolume(const double radius);
+
+/**
+*@brief Рассчитывает выражение a по формуле
+*@param radius - Значение радиуса шара
+*@return Рассчитаное значение a
+*/
+double getArea(const double radius);
+
+/**
+* @brief Считывает вещественное число
+* @return Вещественное число
+*/
+double Input(void);
+
+/**
+* @brief Точка входа в программу =
+* @return Код ошибки
+*/
+int main(void)
+{
+	setlocale(LC_ALL, "Russian");
+	puts("Введите радиус шара");
+	double radius = Input();
+	double bVolume = getVolume(radius);
+	double bArea = getArea(radius);
+	printf_s("Объём шара %lf\n", bVolume);
+	printf_s("Площадь поверхности шара %lf", bArea);
+	return 0;
+}
+
+double getVolume(const double radius)
+{
+	return (4 / 3) * M_PI * pow(radius, 3);
+}
+
+double getArea(const double radius)
+{
+	return 4 * M_PI * pow(radius, 2);
+}
+
+double Input(void)
+{
+	double value;
+	scanf_s("%lf", &value);
+	return value;
+}
