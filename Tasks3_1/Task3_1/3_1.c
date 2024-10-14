d121f52a57215c63861c952bd0db9a526473c05b
Liza-Abuzina <linkschannel220@gmail.com>
2024-10-14 16:44:58 +0300

    Код на проверку

--- Tasks3_1/Task3_1/3_1.c
+++ Tasks3_1/Task3_1/3_1.c
@@ -2,8 +2,16 @@
 #include <locale.h>
 #include <math.h>
 
+/**
+* @brief Точка входа в программу
+* @return 0, в случае успеха
+*/
 int main(void)
 {
 	setlocale(LC_ALL, "Russian");
-	for 
+	for (double x = 2; x < 3.1; x += 0.1)
+	{
+		printf("Результаты вычислений при x = %lf, y = %lf\n",x,3 * sin(sqrt(x)) + 0.39 * x - 3.8);
+	}
+	return 0;
 }
