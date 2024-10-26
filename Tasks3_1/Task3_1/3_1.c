84485c2e78ec2b43bc13bfecc86dbc7bb0ed4eef
Liza-Abuzina <linkschannel220@gmail.com>
2024-10-26 20:36:38 +0300

    Добавила проверку start и end

--- Tasks3_1/Task3_1/3_1.c
+++ Tasks3_1/Task3_1/3_1.c
@@ -36,6 +36,8 @@ int main(void)
 	{
 		if (start <= DBL_EPSILON)
 			printf("%lf Неопределено\n", start);
+		if (start > end)
+			puts("Начало интервала не может быть больше конца");
 		else
 			printf("Результаты вычислений при x = %lf, y = %lf\n", start, 3 * sin(sqrt(start)) + 0.39 * start - 3.8);
 	}
