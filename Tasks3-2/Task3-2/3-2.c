dd6a2b67c30e5260db223381d73ff6503155b8b2
Liza-Abuzina <linkschannel220@gmail.com>
2024-11-09 20:48:19 +0300

    Дополнение к решению

--- Tasks3-2/Task3-2/3-2.c
+++ Tasks3-2/Task3-2/3-2.c
@@ -3,16 +3,33 @@
 #include <errno.h>
 #include <locale.h>
 
+/**
+* @brief Считывает вещественное число
+* @remarks При неправильном вводе программа завершает выполнение.
+* @return Вещественное число
+*/
 double Input(void);
 
+/**
+* @brief Получает на вход число k
+* @return Рекурентное число с заданным k
+*/
 double GetReccurent(double k);
 
+/**
+* @brief Считывает номер последнего члена последовательности
+* @return Сумма рекурентных чисел
+*/
 double GetSumm(double n);
 
+/**
+* @brief Точка входа в программу 
+* @return Код ошибки
+*/
 int main(void)
 {
 	setlocale(LC_ALL, "Russian");
-	puts("Введите количество членов последовательности");
+	puts("Введите номер последнего члена последовательности");
 	double n = Input();
 	printf("Сумма при n = %lf равна %.18lf", n, GetSumm(n));
 }
@@ -27,18 +44,23 @@ double Input(void)
 		perror("Не удалось считать число");
 		exit(EXIT_FAILURE);
 	}
+	if (value <= 0)
+	{
+		errno = EDOM;
+		printf("Количество членов последовательности не может быть < 0");
+		exit(EXIT_FAILURE);
+	}
 	return value;
 }
 
 double GetSumm(double n)
 {
-	double g = 0.666666666666666667;
-	double summ = 0.666666666666666667;
+	double current = 1.0 / 12;
+	double summ = 1.0 / 12;
 	for (double k = 1; k <= n; k++)
 	{
-		printf("g = %.18lf, k = %lf\n", g, k);
-		g *= GetReccurent(k);
-		summ += g;
+		current *= GetReccurent(k);
+		summ += current;
 	}
 	return summ;
 }
