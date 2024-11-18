9e1c3ad110160913394a87d489d6b63691bd720a
Liza-Abuzina <linkschannel220@gmail.com>
2024-11-18 14:52:20 +0300

    Добавила даннные

---
+++ Tasks3-3/Task3-3/3-3.c
@@ -0,0 +1,135 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <float.h>
+
+/*
+*@brief Считывает вещественное число.
+* @remarks При неправильном вводе программа завершает выполнение.
+* @return Вещественное число.
+*/
+double input(void);
+
+/**
+*@brief Проверка интервала.
+* @param start - начальное значение интервала.
+* @param end - конечное значение интервала.
+* @return Значение "true" в случае выполнения условия.
+*/
+double checkinterval(const double start, const double end);
+
+/**
+*@brief Проверка шага.
+* @param При неправильном вводе программа завершает выполнение.
+* @return Значение шага в случае выполнения условия.
+*/
+double checkstep(void);
+
+/**
+*@brief Вычисление значения функции.
+* @param x - значение аргумента функции.
+* @return Значение функции при значении аргумента x.
+*/
+double getfunctionx(const double x);
+
+/**
+*@brief Вычисление значения функции.
+* @param x - значение аргумента функции.
+* @param n - индекс элемента.
+* @return Значение функции при значении аргумента x.
+*/
+double getrecurrfunctionx(const double x, const int n);
+
+/**
+*@brief Вычисление суммы функционального ряда с 1 по n - ный элемент.
+* @param start - начальное значение интервала.
+* @param end - конечное значение интервала.
+* @param eps - заданная точность вычисления.
+* @return Сумма функционального ряда с 1 по n - ный элемент.
+*/
+double getsumm(const double start, const double end, const double eps);
+
+/*
+* @brief Точка входа в программу.
+* @return Код ошибки.
+*/
+int main(void)
+{
+    setlocale(LC_ALL, "Russian");
+    const double eps = pow(10, -4);
+    printf("Введите начало интервала: ");
+    double start = input();
+    printf("Введите конец интервала: ");
+    double end = input();
+    checkinterval(start, end);
+    printf("Введите шаг на интервале: ");
+    double step = checkstep();
+    double x = start;
+    printf("%-10s %-20s %-20sn", "x", "Значение функции", "Значение функционального ряда\n");
+    while (x <= end + DBL_EPSILON)
+    {
+        printf("%-10.2lf | %-20.10lf | %-20.10lf\n", x, getfunctionx(x), getsumm(start, end, eps));
+        x += step;
+    }
+    return 0;
+}
+
+double input(void)
+{
+    double value = 0.0;
+    if (scanf_s("%lf", &value) != 1)
+    {
+        errno = EIO;
+        perror("Ошибка ввода числового значения: ");
+        exit(EXIT_FAILURE);
+    }
+    return value;
+}
+
+double checkinterval(const double start, const double end)
+{
+    if (end - start < DBL_EPSILON)
+    {
+        errno = EIO;
+        perror("Ошибка ввода числового значения: ");
+        exit(EXIT_FAILURE);
+    }
+}
+
+double checkstep(void)
+{
+    double step = input();
+    if (step < DBL_EPSILON)
+    {
+        errno = EIO;
+        perror("Ошибка ввода числового значения: ");
+        exit(EXIT_FAILURE);
+    }
+    return step;
+}
+
+double getfunctionx(const double x)
+{
+    return pow(3, x);
+}
+
+double getrecurrfunctionx(const double x, const int n)
+{
+    return (x + log(3*x))/(n+1);
+}
+
+double getsumm(const double start, const double end, const double eps)
+{
+    double curr = start;
+    double summ = 0.0;
+    int n = 0;
+    while (curr > eps - DBL_EPSILON)
+    {
+        summ += curr;
+        curr *= getrecurrfunctionx(start, n);
+        n++;
+    }
+    return summ;
+}
