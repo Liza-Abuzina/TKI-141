bc6753316465ede7378b1a4b0463f597967aab74
Liza-Abuzina <linkschannel220@gmail.com>
2024-12-23 12:20:14 +0300

    база под задание

---
+++ Tasks4-2/Task4-2/4-2.c
@@ -0,0 +1,223 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
+
+/**
+* @brief Заменяет второй элемент массива максимальным отрицательным элементом
+* @param array - указатель на массив целых чисел
+* @param n - размер массива
+*/
+void replacesecondelementwithmaxnegative(int* array, const size_t n);
+
+/**
+* @brief Массив
+* @param array - массив
+* @param n - размер массива
+* @return Выводит пользователю массив
+*/
+void printarray(const int* array, const size_t n);
+
+/**
+* @brief Заполнение массива n - количеством рандомных элементов в интервале.
+* @param array - массив.
+* @param n - размер массива.
+* @return Заполненый массив.
+*/
+void fillarrayrandomly(int* array, const size_t n);
+
+/**
+* @brief Заполнение массива n - количеством элементов полученных от пользователя.
+* @param array - массив
+* @param n - размер массива
+* @return Заполненый массив
+*/
+void fillarraymanually(int* array, const size_t n);
+
+/**
+* @brief Проверка корректности ввода интервала.
+* @param start - начало интервала.
+* @param end - конец интервала.
+* @return Значение "true" в случае усепшной проверки.
+*/
+void checkinterval(int const start, int const end);
+
+/**
+* @brief Проверка иницилизации массива.
+* @param array - массив
+* @return Значение "true" в случае усепшной проверки.
+*/
+void checkarray(const int* array);
+
+/**
+* @brief Массив
+* @param n - размер массива
+* @return Создание нового массива
+*/
+int* getnewarray(const int n);
+/**
+* @brief Считывает целочисленное значение.
+* @remarks При неправильном вводе программа завершает выполнение.
+* @return Целочисленное значение.
+*/
+int inputvalue(void);
+
+/**
+* @brief Считывает целочисленное значение.
+* @remarks Проверяет полученное значение на ноль, при соответствии программа завершает выполнение.
+* @return Целочисленное значение.
+*/
+int abovezero(void);
+
+/**
+* @brief Выбор исполняемой функции
+* @brief Заполнить массив рандомными числами
+* @brief Заполнить массив самостоятельно
+*/
+enum Choise
+{
+    Fillrandomly = 1,
+    Fillmanually = 2,
+};
+
+/**
+* @brief Точка входа в программу.
+* @return 0, в случае успеха.
+*/
+int main(void)
+{
+    setlocale(LC_ALL, "Russian");
+    printf("Введите размер массива: ");
+    size_t n = (size_t)abovezero();
+    printf("Если вы хотите заполнить массив рандомными числами, то введите (%d), иначе (%d): ", Fillrandomly, Fillmanually);
+    int choice = inputvalue();
+    int* array = getnewarray(n);
+    switch ((enum Choise)choice)
+    {
+    case Fillrandomly:
+        fillarrayrandomly(array, n);
+        break;
+    case Fillmanually:
+        fillarraymanually(array, n);
+        break;
+    default:
+        puts("Ошибка ввода номера операции.");
+        free(array);
+        return 1;
+    }
+    printf("Исходный массив: \n");
+    printarray(array, n);
+    replacesecondelementwithmaxnegative(array, n);
+    printf("Массив после замены второго элемента:\n");
+    for (size_t i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    free(array);
+    return 0;
+}
+
+void replacesecondelementwithmaxnegative(int* array, const size_t n)
+{
+    int maxnegative = -1000;
+    for (size_t i = 0; i < n; i++)
+    {
+        if (array[i] < 0 && array[i] > maxnegative)
+        {
+            maxnegative = array[i];
+        }
+    }
+    if (maxnegative < 0 && n > 1)
+    {
+        array[1] = maxnegative;
+    }
+}
+
+void printarray(const int* array, const size_t n)
+{
+    checkarray(array);
+    for (size_t i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+void fillarrayrandomly(int* array, const size_t n)
+{
+    checkarray(array);
+    srand(time(NULL));
+    printf("Введите начало интервала: ");
+    int start = inputvalue();
+    printf("Введите конец интервала: ");
+    int end = inputvalue();
+    checkinterval(start, end);
+    for (size_t i = 0; i < n; i++)
+    {
+        array[i] = rand() % (end - start + 1) + start;
+    }
+}
+
+void fillarraymanually(int* array, const size_t n)
+{
+    checkarray(array);
+    for (size_t i = 0; i < n; i++)
+    {
+        printf("Введите элемент %zu: ", i + 1);
+        scanf_s("%d", &array[i]);
+    }
+}
+
+void checkinterval(int const start, int const end)
+{
+    if (start > end)
+    {
+        errno = EIO;
+        perror("Ошибка ввода интервала: ");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void checkarray(const int* array)
+{
+    if (array == NULL)
+    {
+        errno = EIO;
+        perror("Массив не инициализирован: ");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int* getnewarray(const int n)
+{
+    int* array = (int*)malloc(n * sizeof(int));
+    checkarray(array);
+    return array;
+}
+
+int inputvalue(void)
+{
+    int value = 0;
+    if (scanf_s("%d", &value) != 1)
+    {
+        errno = EIO;
+        perror("Ошибка ввода числового значения: ");
+        exit(EXIT_FAILURE);
+    }
+    return value;
+}
+
+int abovezero(void)
+{
+    int value = inputvalue();
+    if (value <= 0)
+    {
+        errno = EIO;
+        perror("Ошибка ввода числового значения (оно не может быть меньше или равным нулю): ");
+        exit(EXIT_FAILURE);
+    }
+    return value;
+}
